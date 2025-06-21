bd4c68dbc021ed6afa6d5a948aef044efa598723
ridhoarrosyid <muhammadridhoarrosyid@gmail.com>
2025-06-21 15:11:08 +0700

    learning hapi framework

---
+++ routes.js
@@ -0,0 +1,49 @@
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage";
+    },
+  },
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut";
+    },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About page";
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method";
+    },
+  },
+  {
+    method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+      if (lang == "id") return `Halo kak ${name}`;
+      return `Hallo ${name}`;
+    },
+  },
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "Halaman tidak ditemukan";
+    },
+  },
+];
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,15 @@
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+const init = async () => {
+  const server = Hapi.server({
+    host: "localhost",
+    port: 5000,
+  });
+
+  server.route(routes);
+  await server.start();
+  console.log(`server berjalan pada ${server.info.uri}`);
+};
+
+init();
